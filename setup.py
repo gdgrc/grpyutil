a4a482d1683bcd30ce264393068f4cc8abd7236b
hezijun <zijunhe@digitalgd.com.cn>
2021-06-02 23:03:08 +0800

    add

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ find_packages_list = find_packages()
 print(find_packages_list)
 setup(
     name="grpyutil",
-    version="0.35",
+    version="0.36",
     description="utils module",
     author="ZJ",
     # url="",
