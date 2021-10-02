098a23246631d5706f721db8a6b780c0dee33b8d
hezijun <zijunhe@digitalgd.com.cn>
2021-10-02 17:38:43 +0800

    add

--- grpyutil/common/check_rules/time.py
+++ grpyutil/common/check_rules/time.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta
-
+import datetime  
 
 class DateTime(object):
     def __init__(self, instr):
@@ -8,39 +7,40 @@ class DateTime(object):
 
         if isinstance(instr, datetime.datetime):
             self.datetime = instr
+            return
         
 
         try:
-            self.datetime = datetime.strptime(instr, "%Y-%m-%d %H:%M:%S")
+            self.datetime = datetime.datetime.strptime(instr, "%Y-%m-%d %H:%M:%S")
             self.datetime_type = 1
         except Exception as e:
 
             try:
-                self.datetime = datetime.strptime(instr, "%Y-%m-%d %H:%M")
+                self.datetime = datetime.datetime.strptime(instr, "%Y-%m-%d %H:%M")
                 self.datetime_type = 2
             except Exception as e:
                 try:
-                    self.datetime = datetime.strptime(instr, "%Y-%m-%d")
+                    self.datetime = datetime.datetime.strptime(instr, "%Y-%m-%d")
                     self.datetime_type = 3
                 except Exception as e:
                     try:
-                        self.datetime = datetime.strptime(instr, "%Y-%m")
+                        self.datetime = datetime.datetime.strptime(instr, "%Y-%m")
                         self.datetime_type = 4
                     except Exception as e:
                         try:
-                            self.datetime = datetime.strptime(instr, "%Y-%m-%d %H:%M:%S.%f")
+                            self.datetime = datetime.datetime.strptime(instr, "%Y-%m-%d %H:%M:%S.%f")
                             self.datetime_type = 5
                         except Exception as e:
 
                             try:
-                                self.datetime = datetime.fromtimestamp(int(instr))
+                                self.datetime = datetime.datetime.fromtimestamp(int(instr))
                                 self.datetime_type = 6
                             except Exception as e:
                                 try:
                                     timestamp_ms = int(instr)
                                     if timestamp_ms < 1000:
                                         raise Exception("timestamp is not timestamp ms. %d" % timestamp_ms)
-                                    self.datetime = datetime.fromtimestamp(timestamp_ms / 1000)
+                                    self.datetime = datetime.datetime.fromtimestamp(timestamp_ms / 1000)
                                     self.datetime_type = 7
                                 except Exception as e:
                                     raise Exception("unknow type: %s error : %s" % (type(instr), e))
@@ -55,7 +55,7 @@ class DateTime(object):
         return self.datetime.strftime("%Y-%m-%d")
 
     def get_the_first_day_of_week(self):
-        return DateTime((self.datetime - timedelta(days=self.datetime.isocalendar()[2] - 1)).strftime("%Y-%m-%d"))
+        return DateTime((self.datetime - datetime.timedelta(days=self.datetime.isocalendar()[2] - 1)).strftime("%Y-%m-%d"))
 
     def equal(self, d):
         # delta = self.sub(d)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ find_packages_list = find_packages()
 print(find_packages_list)
 setup(
     name="grpyutil",
-    version="0.57",
+    version="0.58",
     description="utils module",
     author="ZJ",
     # url="",
